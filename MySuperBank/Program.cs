e6da94e5a77679fcef7f5dbc250f656407d439b9
github username <56710845+m-delarosa@users.noreply.github.com>
2021-01-01 12:43:37 -0700

    Add BankAccount and Transaction classes.

--- MySuperBank/Program.cs
+++ MySuperBank/Program.cs
@@ -6,7 +6,18 @@ namespace MySuperBank
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var account = new BankAccount("Kendra", 1000);
+            Console.WriteLine($"Account {account.Number} was created for " +
+                $"{account.Owner} with {account.Balance}");
+
+            account.MakeWithdrawal(120, DateTime.Now, "Hammock");
+            Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
         }
     }
 }
